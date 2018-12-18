c9d34537772c80b3d9fa90c37e53e0f339026eaf
tangjiamei <tangjiamei@wdai.com>
2018-12-18 16:34:52 +0800

    dsafds

--- configServer/src/main/java/com/example/demo/ConfigServerApplication.java
+++ configServer/src/main/java/com/example/demo/ConfigServerApplication.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+ import org.springframework.boot.autoconfigure.SpringBootApplication;
+ import org.springframework.cloud.config.server.EnableConfigServer;
+ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @EnableConfigServer
